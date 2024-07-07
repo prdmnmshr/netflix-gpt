864fcf78597af551f36cff3af722cbc02410bbd5
prdmnmshr <prdmnmshr@gmail.com>
2024-07-07 23:22:24 +0530

    added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Browse from './Browse'
 import Login from './Login'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Body = () => {
+  const dispatch = useDispatch();
     const appRouter = createBrowserRouter([
         {
             path: "/",
--- src/components/Header.js
+++ src/components/Header.js
@@ -30,9 +30,13 @@ const Header = () => {
     });
   }
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex'>
       <img className='w-36' 
       src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
+    <div>
+      <img src="https://occ-0-3646-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229" alt="" />
+      <button>(Sign out)</button>
+    </div>
     </div>
   )
 }
